485d887ee3f648bfb98a92bede4a741efaa00454
wangguojun <itwanggj@163.com>
2016-01-12 12:43:49 +0800

    毛琦第二次作业+读书报告

---
+++ nb15032毛琦/毛琦OpenGL第一次作业/opengl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL_Mao
+//
+//  Created by 毛毛 on 15/12/16.
+//  Copyright © 2015年 毛毛. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ nb15032毛琦/毛琦OpenGL第一次作业/opengl/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  OpenGL_Mao
+//
+//  Created by 毛毛 on 15/12/16.
+//  Copyright © 2015年 毛毛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
